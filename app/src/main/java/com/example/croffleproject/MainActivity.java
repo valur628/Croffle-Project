8c9a9f8455c285ed4e59b0e0ff8257db756b08e5
JeongMin Choi <tlsdnjfwjdals159@gmail.com>
2022-05-08 13:59:31 +0900

    [UI] #5 : 설정페이지 UI 완, 그래프 설정 중

--- app/src/main/java/com/example/croffleproject/MainActivity.java
+++ app/src/main/java/com/example/croffleproject/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.croffleproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), StatsPageActivity.class));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SettingPageActivity.class));
+            }
+        });
     }
 }
